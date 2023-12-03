a05ed06d22fef8b222d7371ef788c2fbc9f02244
Arbitel <2641332+chadleong@users.noreply.github.com>
2023-12-03 18:23:25 +0100

    chapter 5

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,7 +4,7 @@ import { inter } from '@/app/ui/fonts';
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode;
+  readonly children: React.ReactNode;
 }) {
   return (
     <html lang="en">
